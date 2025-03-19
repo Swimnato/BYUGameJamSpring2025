591fe197f6d55952b3737bea1a3fc0a53c76c6d9
Swimnato <66497456+Swimnato@users.noreply.github.com>
2025-03-19 00:38:07 -0600

    added npc cooldown

--- Scripts/scr_npc.gd
+++ Scripts/scr_npc.gd
@@ -1,8 +1,12 @@
 extends Node3D
 
 @export var text = "Howdy Partner!";
+@export var cooldown = 5; #cooldown for speaking to NPC
+
+var lastTimeSpokenTo = 0;
 
 func _on_area_3d_body_entered(body: Node3D) -> void:
-	if(body.name == "Player"):
+	if(body.name == "Player" && (lastTimeSpokenTo == 0 || Time.get_ticks_msec() - lastTimeSpokenTo >= cooldown * 1000)):
+		lastTimeSpokenTo = Time.get_ticks_msec();
 		print("Todo: Dialog   " + text);
 		body.stop_and_face_npc(self, text);
