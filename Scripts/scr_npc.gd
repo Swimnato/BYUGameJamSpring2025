6090101d6407df25f7ff458a0ef45d7ca927f927
tpurba <tpurba@byu.edu>
2025-03-21 11:50:49 -0600

    Finished dialogue template

--- Scripts/scr_npc.gd
+++ Scripts/scr_npc.gd
@@ -1,42 +1,44 @@
 extends Node3D
 
-@export var cooldown = 500; #cooldown for speaking to NPC
+@export var cooldown = 1;  # Cooldown for speaking to NPC in seconds
 
-var lastTimeSpokenTo = 0;
+var lastTimeSpokenTo = 0;  # Keeps track of the last time the player spoke to the NPC
 var speed;
 var current_state = IDLE;
 
-
-var is_chatting = false;
 var player;
 var player_in_chat_zone = false;
-enum{
+
+enum {
 	IDLE,
 	NEW_DIR,
 	MOVE
 }
 
+func _ready() -> void:
+	$Dialogue.connect("dialogue_finished", Callable(self, "_on_dialogue_dialogue_finished"));
+
 func _process(delta: float) -> void:
-	if(Input.is_action_just_pressed("chat") && (player_in_chat_zone) && !$Dialogue.d_active && Time.get_ticks_msec() - lastTimeSpokenTo > cooldown):
-		print("chatting with npc");
-		$Dialogue.start();
-		is_chatting = true;
-		current_state = IDLE;
+	if Input.is_action_just_pressed("chat") and player_in_chat_zone:
+		if lastTimeSpokenTo == 0 or Time.get_ticks_msec() - lastTimeSpokenTo >= cooldown * 1000:
+			print("chatting with npc");
+			$Dialogue.start();  # Start the dialogue if the cooldown has passed
+			lastTimeSpokenTo = Time.get_ticks_msec();  # Update the last time spoken to NPC
+			current_state = IDLE;
+		else:
+			var remaining_time = cooldown - (Time.get_ticks_msec() - lastTimeSpokenTo) / 1000;
+			print("You must wait " + str(remaining_time) + " more seconds to chat with the NPC.");
 		
-
 func _on_area_3d_body_entered(body: Node3D) -> void:
-	if(body.name == "Player"):
+	if body.name == "Player":
 		player_in_chat_zone = true;
-		body.stop_and_face_npc(self);
-		
-
+		$TalkPrompt.visible = true;
 
 func _on_speaking_zone_body_exited(body: Node3D) -> void:
-	if(body.name == "Player"):
+	if body.name == "Player":
 		player_in_chat_zone = false;
-		
-
+		$TalkPrompt.visible = false;
 
 func _on_dialogue_dialogue_finished() -> void:
+	print('Dialogue finished')
 	lastTimeSpokenTo = Time.get_ticks_msec();
-	is_chatting = false;
