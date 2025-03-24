77c3ab8ead51cd49fee1f9f409eac4c92592abc0
Swimnato <66497456+Swimnato@users.noreply.github.com>
2025-03-23 23:24:11 -0600

    added audio track playing code for inside

--- Scenes/levels/audio_stream_player.gd
+++ Scenes/levels/audio_stream_player.gd
@@ -1,19 +1,26 @@
 extends AudioStreamPlayer
 
-var musicList:Array = [preload("res://Audio/Music/Song2Loop.wav")];
+var musicList:Array = [preload("res://Audio/Music/Song2Loop.wav"), preload("res://Audio/Music/Song1Loop.wav")];
 var currentSong = 0;
 var prevSong;
 var lastAttempt = 0;
 var delayAttempt = 100;
 var volume = -20;
 @export var shouldPlay = true;
+@onready var player = $"../Player";
 
 func _ready():
 	volume_db = volume;
 
 func _process(delta: float) -> void:
+	if(player.floorType == player.surfaceType.GRASS or player.floorType == player.surfaceType.LILLYPAD):
+		currentSong = 0;
+	else:
+		currentSong = 1;
 	if((!playing or prevSong != currentSong) and shouldPlay):
 		print(currentSong);
+		if(playing):
+			stop();
 		prevSong = currentSong;
 		stream = musicList[currentSong];
 		play()
